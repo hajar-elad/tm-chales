0ac17a664c0f41169c5cc9b3e0446235518d285c
hajar-elad <hajar.elad@gmail.com>
2025-10-20 22:58:51 +0200

    Add files via upload

---
+++ scripts/chales.js
@@ -0,0 +1,48 @@
+import {products} from '../data/products.js';    
+
+
+
+export function listProducts(category){
+    let html = '';
+    products.forEach((product) => {
+       if(product.category === category){
+           html += `
+        <div class="product-container product-container-${product.id}">
+                <img class="product-image js-product-image" src="${product.image}"  data-product-id=${product.id}>
+                <div class="product-infos">
+                    <div class="product-name">${product.name}</div>
+                    <div class="product-price">${(product.price).toFixed(2)} DHS</div>
+                </div>
+                <button class="add-to-cart-btn" data-product-id=${product.id}>Ajouter Au Panier</button>
+            </div>`
+       }
+    })
+    return html;
+}
+
+
+export function createModal(productId){
+
+    let html = '';
+    
+
+    products.forEach((product) => {
+        if(productId === product.id){
+
+             html =`<div class="modal">
+                <img src="${product.image}" class="modal-image">
+                <div class="modal-infos">
+                    <div class="modal-name">${product.name}</div>
+                    <div class="modal-price">${(product.price).toFixed(2)} DHS</div>
+                </div>
+                <button class="close-modal js-close-modal">X</button>
+
+            </div>`
+         }
+    })
+
+    //Create The Modal
+    document.querySelector('.modal-container-hide')
+       .innerHTML = html;
+}
+
